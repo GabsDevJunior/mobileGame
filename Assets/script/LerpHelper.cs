dcb75828789c14ed5f1fbd4be7bd3447ea5ff959
GabsDevJunior <138792131+GabsDevJunior@users.noreply.github.com>
2025-06-20 12:14:42 -0300

    estou fazendo este comit para verem oq esta de errado

--- Assets/script/LerpHelper.cs
+++ Assets/script/LerpHelper.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class LerpHelper : MonoBehaviour
 {
-<<<<<<< Updated upstream
     public float lerpSpeed;
     public Transform target;
-=======
-
-    public Transform target;
-
-    public float lerpSpeed;
->>>>>>> Stashed changes
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,10 +14,6 @@ public class LerpHelper : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.Lerp(transform.position, target.position, lerpSpeed
-<<<<<<< Updated upstream
-        * Time.deltaTime * 2f);
-=======
         * Time.deltaTime);
->>>>>>> Stashed changes
     }
 }
